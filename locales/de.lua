0c5e1a5a302da13501b2108cb461933508adc476
MilyonJames <clement.dolo@gmail.com>
2023-10-23 09:20:33 +0200

    clean locales + de locale (by xionerazer)

---
+++ locales/de.lua
@@ -0,0 +1,10 @@
+Locales["de"] = {
+	main_menu = "Hauptmenu",
+	no_desc = "keine Beschreibung",
+	
+    entity_type_name_0 = "Personal",
+    entity_type_name_1 = "Unbekannter",
+    entity_type_name_2 = "Fahrzeug",
+    entity_type_name_3 = "Objekt",
+    entity_type_name_in_vehicle = "Fahrzeug"
+}
